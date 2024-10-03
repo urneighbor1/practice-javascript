f0a684360fa74e2935f50872f18fdab355e45547
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2024-10-03 05:21:03 +0000

    10-3

--- main.js
+++ main.js
@@ -1,62 +1,23 @@
-document.getElementById("text").style.color = "red";
-
-document.querySelector("h1").style.color = "red";
-
-// document.getElementById("text").style.color = "red";
-/* これは、ドキュメントの中の"text"というidを持つ
-  最初に見つかったタグの文字色を"red"にする
+/*
+  JavaScriptのプログラムとHTMLのタグはどこで対応しているか
+  タグについているidで持ってこれる
+  タグの中にある付加情報のことを属性という
+  console.log()はC言語でいうところのprintf()のようなもの
 */
 
-function changeColor() {
-  document.getElementById("text").style.color = "green";
+/** @type {HTMLInputElement} */
+const monthInput = document.getElementById("month");
+/** @type {HTMLInputElement} */
+const dayInput = document.getElementById("day");
+
+const p = document.createElement("p");
+document.body.appendChild(p);
+
+function errorCheck() {
+  console.log(monthInput);
+  console.log(dayInput);
 }
 
-const countElement = document.getElementById("counter");
-
-let count = 1;
-function increaseCount() {
-  count++;
-  if (count > 5) {
-    count = 1;
-  }
-  countElement.innerText = count;
-}
-
-function decreaseCount() {
-  count--;
-  if (count < 1) {
-    count = 5;
-  }
-  countElement.innerText = count;
-}
-
-{
-  // EventListener
-  let counter_num = 1;
-  const counterDisplay = document.createElement("p");
-  counterDisplay.innerText = "1";
-
-  const increaseCounter = document.createElement("button");
-  increaseCounter.innerText = "増やすボタン";
-  increaseCounter.addEventListener("click", () => {
-    counter_num++;
-    if (counter_num > 5) {
-      counter_num = 1;
-    }
-    counterDisplay.innerText = counter_num;
-  });
-
-  const decreaseCounter = document.createElement("button");
-  decreaseCounter.innerText = "減らすボタン";
-  decreaseCounter.addEventListener("click", () => {
-    counter_num--;
-    if (counter_num < 1) {
-      counter_num = 5;
-    }
-    counterDisplay.innerText = counter_num;
-  });
-
-  const div = document.createElement("div");
-  div.append(increaseCounter, decreaseCounter, counterDisplay);
-  document.body.appendChild(div);
+function showBirth() {
+  p.innerText = `${month.value}月${dayInput.value}日`;
 }
