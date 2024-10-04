2e9f8e91d160e6e1a7a44df6504a79b10dec4af9
urneighbor1 <157030786+urneighbor1@users.noreply.github.com>
2024-10-04 05:59:14 +0000

    10-4 写経

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
   タグの中にある付加情報のことを属性という
   console.log()はC言語でいうところのprintf()のようなもの
 */
-
 /** @type {HTMLInputElement} */
 const monthInput = document.getElementById("month");
 /** @type {HTMLInputElement} */
@@ -14,10 +13,22 @@ const p = document.createElement("p");
 document.body.appendChild(p);
 
 function errorCheck() {
-  console.log(monthInput);
-  console.log(dayInput);
+  const month = monthInput.value;
+  const day = dayInput.value;
+  if (/* isNaN(month) || */ !(1 <= month && month <= 12)) {
+    alert("適切な値ではありません");
+    return;
+  }
+  /*
+  const date = new Date(2000, month - 1, day);
+  if (date.getMonth() + 1 != month || date.getDate() != day) {
+    alert("適切な値ではありません");
+    return;
+  }
+    */
+  console.log(`month: ${monthInput.value}, day: ${dayInput.value}`);
 }
 
 function showBirth() {
-  p.innerText = `${month.value}月${dayInput.value}日`;
+  p.innerText = `${monthInput.value}月${dayInput.value}日`;
 }
